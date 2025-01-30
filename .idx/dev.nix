947d494df54700181e27894a4aa34fbdbb6b9c1b
Nate <n83b@users.noreply.github.com>
2025-01-30 23:00:27 +1030

    Create dev.nix
    Config file to install php, wp-cli, myql and install wordpress

---
+++ .idx/dev.nix
@@ -0,0 +1,59 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.php
+    pkgs.wp-cli
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  services = {
+    mysql = {
+      enable = true;
+    };
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ ".idx/dev.nix" "install-wordpress.sh" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        install-wordpress = ''
+          sleep 5
+          mysql -V
+          chmod +x install-wordpress.sh
+          bash ./install-wordpress.sh'';
+        run-server = "php -S localhost:3000 index.php";
+      };
+    };
+  };
+}
